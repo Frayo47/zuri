4e1407ffe22ed39b7669e10f35b5d17decd1c0b3
Friday <81017092+Frayo47@users.noreply.github.com>
2021-08-22 00:43:32 +0100

    Add files via upload

---
+++ Name.py
@@ -0,0 +1,1 @@
+print("oguche friday")
---
+++ resume.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Template Resume</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+</head>
+<body class="bg-light">
+    <div class="container p-5">
+        <div class="row">
+            <div class="col-lg-4 bg-dark text-white text-center py-4">
+                <div class="header-left">
+                    <img src="img-1.jpg" alt="" class="img-thumbnail rounded-circle mb-2">
+                    <hi class="display-5">Oguche Friday</hi>
+                    <h4 class="lead text-uppercase text-white-50 mb-4">Backend Developer</h4>
+                </div>
+
+                <div>
+                    <h5 class="text-uppercase bg-white text-dark py-2 rounded-pi11">contact</h5>
+
+                    <ul class="list-unstyled text-white-50 ml-5 py-2 text-left">
+                        <li class="list-item">
+                            <i class="fas fa-mobile-alt mx-4"></i>
+                            +234 818 045 2904
+                        </li>
+                        <li class="list-item">
+                            <i class="fas fa-envelope-open-text mx-4"></i>
+                            oguchefriday7@gmail.com
+                        </li>
+                        <li class="list-item">
+                        <i class="fas fa-blog mx-4"></i>
+                            www.frayo.hashnode.dev.com
+                        </li>
+                        <li class="list-item">
+                            <i class="fas fa-map-marker-alt mx-4"></i>
+                            Mellanby Hall, University of Ibadan, Ibadan
+                        </li>
+                    </ul>
+                </div>
+                        <a href="index.php"><button>Contact Me</button></a>
+
+                <div>
+                    <h5 class="text-uppercase bg-white text-dark py-2 rounded-pi11">skills</h5>
+                    <ul class="list text-white-50 ml-5 py-2 text-left text-capitalize">
+                        <li class="list-item">Python</li>
+                        <li class="list-item">Problem solving skills</li>
+                        <li class="list-item">Adaptability</li>
+                        <li class="list-item">Interpersonal communication skills</li>
+                    </ul>
+                </div>
+                <div>
+                    <h5 class="text-uppercase bg-white text-dark py-2 rounded-pi11">education</h5>
+                    <ul class="list text-white-50 ml-5 py-2 text-left text-capitalize">
+                        <li class="list-item">BSc Animal Science (2016-2021)</li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="col-lg-8 bg-light text-dark py-4 px-5">
+                <div class="header-right">
+                    <h4 class="text-uppercase">profile</h4>
+                    <hr>
+                    <p>A Python Developer offering exceptional analytical and critical thinking skills. I Deliver a proactive approach, great work ethic, and also the ability to function well in fast paced/deadline-driven team environments.</p>
+                </div>
+
+                <div>
+                    <h4 class="text-uppercase">experience</h4>
+                    <hr>
+
+                    <ul class="list">
+                        <li class="list-item">
+                            <h5 class="display-6 text-uppercase">python developer intern</h5>
+                            <h6 class="text-uppercase text-black-50">zuri internship / 2021-present</h6>
+                            <li class="list-item">Designed robust, scalable, secure, and globalized web-based application</li>
+                            <li class="list-item">Performed data entry and other clerical work for project completion.</li>
+                             <a href="https://.zuri.team/"><button>click to go to zuri</button></a>                     
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    <div>
+</body>
+</html>
